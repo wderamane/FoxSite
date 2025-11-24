8446304f84025a2e1d9b6272a733c8e3d7609cd6
FoxVerse Ent <deramane.waren01@gmail.com>
2025-11-24 12:03:45 +1100

    #4

--- foxverse-entreprise/vite.config.js
+++ foxverse-entreprise/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/FoxSite/",
+  base: "/FoxSite/",   // 🔥 IMPORTANT : Nom EXACT du dépôt GitHub
 });
